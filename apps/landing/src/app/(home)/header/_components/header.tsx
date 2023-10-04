6035fd4500b1d63f0ff5c4785ada8c2c3b1b75c1
Alan Bahena <alanbahena1@gmail.com>
2023-10-04 11:53:41 -0700

    feat: added header copywrite section for desktop

--- apps/landing/src/app/(home)/header/_components/header.tsx
+++ apps/landing/src/app/(home)/header/_components/header.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import { Button } from '@biowelt/custom-react'
+import Arrow from '../../../../../public/assets/arrow-right.png'
 import Navbar from './navbar'
 import clasess from './header.module.scss'
 
@@ -5,7 +8,24 @@ export default function Header(): JSX.Element {
   return (
     <div className={clasess.container}>
       <Navbar />
-      <div>Header</div>
+      <header>
+        <div className={clasess.copywrite}>
+          <h1>
+            Una nueva forma de administrar tu negocio, lean restaurant <span>tu ventaja</span>
+          </h1>
+          <p>
+            Automatiza toda las lineas de producción de tu restaurante partiendo desde un control de
+            inventario en tiempo real desde cualquier dispositivo conectado a internet.{' '}
+          </p>
+          <Button className={clasess.btn} link="/dashboard">
+            <div className={clasess['btn-content']}>
+              <p>Empezar demo</p>
+              <Image alt="arrow" height={11} priority src={Arrow} width={17} />
+            </div>
+          </Button>
+        </div>
+        <div className={clasess.image}>Image</div>
+      </header>
     </div>
   )
 }
