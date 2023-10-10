30f68c6ea9bf9a48d3da2db6b796011ee3d0901a
Alan Bahena <alanbahena1@gmail.com>
2023-10-10 10:43:11 -0700

    feat: added benefits header for desktop

--- apps/landing/src/app/(home)/benefits/_components/benefits.tsx
+++ apps/landing/src/app/(home)/benefits/_components/benefits.tsx
@@ -2,12 +2,19 @@ import clasess from './benefits.module.scss'
 
 export default function Benefits(): JSX.Element {
   return (
-    <div className={clasess.container}>
+    <section className={clasess.container}>
       <div className={clasess.box}>
-        <div className={clasess['box-header']}>Header</div>
+        <div className={clasess['box-header']}>
+          <p>Una plataforma todo en uno de analíticas, datos y automatización en la nube</p>
+          <hr />
+          <h2>
+            ¿Estás listo para <span>transformar</span> tu restaurante y aumentar tus
+            <span> ganancias?</span>
+          </h2>
+        </div>
         <div className={clasess['box-image']}>Image</div>
         <div className={clasess['box-text']}>Text</div>
       </div>
-    </div>
+    </section>
   )
 }
