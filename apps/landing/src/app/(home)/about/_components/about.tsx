edd91a7178968e92aa418233d5714f3618a37062
Alan Bahena <alanbahena1@gmail.com>
2023-10-18 10:55:13 -0700

    feat: added call to action seciton with responsive design

--- apps/landing/src/app/(home)/about/_components/about.tsx
+++ apps/landing/src/app/(home)/about/_components/about.tsx
@@ -1,10 +1,12 @@
 import clasess from './about.module.scss'
 import Transform from './transform'
+import Cta from './cta'
 
 export default function About(): JSX.Element {
   return (
     <section className={clasess.container}>
       <Transform />
+      <Cta />
     </section>
   )
 }
