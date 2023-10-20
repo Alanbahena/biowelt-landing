9e8b62f069581f4cf7be869386dc55624ee551b3
Alan Bahena <alanbahena1@gmail.com>
2023-10-19 17:54:57 -0700

    feat:added copywrite footer section with responsive design

--- apps/landing/src/app/(home)/footer/_components/footer.tsx
+++ apps/landing/src/app/(home)/footer/_components/footer.tsx
@@ -1,3 +1,8 @@
+import Image from 'next/image'
+import { Button } from '@biowelt/custom-react'
+import Arrow from '../../../../../public/assets/arrow-right.png'
+import Heart from '../../../../../public/assets/heart.png'
+import Logo from '../../../../../public/assets/Logo.png'
 import clasess from './footer.module.scss'
 
 export default function Footer(): JSX.Element {
@@ -5,9 +10,30 @@ export default function Footer(): JSX.Element {
     <>
       <hr className={clasess.hr} />
       <footer className={clasess.container}>
-        <div className={clasess.logo}>Logo</div>
-        <div className={clasess.Cta}>CTA</div>
-        <div className={clasess.copywrite}>COPY</div>
+        <div className={clasess.logo}>
+          <Image alt="Biowelt logo" className={clasess['logo-img']} priority src={Logo} />
+          <p>
+            Una nueva forma de administrar tu negocio, lean restaurant <span>tu ventaja</span>
+          </p>
+        </div>
+        <div className={clasess.Cta}>
+          <p>
+            Listo para comenzar? <br />
+            Agenda una nueva demo ahora mismo
+          </p>
+          <Button className={clasess.btn}>
+            <div className={clasess['btn-content']}>
+              <p>Empezar Demo</p>
+              <Image alt="arrow" height={11} priority src={Arrow} width={17} />
+            </div>
+          </Button>
+        </div>
+        <div className={clasess.copywrite}>
+          <p>
+            Hecho con <Image alt="Heart" className={clasess.heart} src={Heart} /> en méxico
+          </p>
+          <span>Copyright © Biowelt 2023. </span>
+        </div>
       </footer>
     </>
   )
